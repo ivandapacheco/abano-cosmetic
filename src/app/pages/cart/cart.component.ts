23fc2d9d064942c2a017654da6ca9cbbaadfe360
Ivan Pacheco <pachecocabarca2001@gmail.com>
2024-08-14 10:26:20 -0500

    Correciones en el header module

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,22 +1,23 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject  } from '@angular/core';
 import { CartService } from '../../services/cart.service'
 import { IProduct } from '../../models/products.model'; 
+import { RouterLink } from '@angular/router';
 
 
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './cart.component.html',
-  styleUrl: './cart.component.scss'
+  styleUrl: './cart.component.scss',
 })
 export class CartComponent {
 
   _cartService = inject(CartService);
   
   cartProducts: IProduct[] = [];
-
+  numberProduct:number = 1;
 
   llenarArrayProduct(){
     this.cartProducts = this._cartService.getCartProducts(); 
--- src/app/pages/header/header.module.ts
+++ src/app/pages/header/header.module.ts
@@ -1,24 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AboutComponent } from './about/about.component';
 import { HeaderComponent } from './header/header.component';
 import { LogoComponent } from './logo/logo.component';
 import { CartComponent} from '../cart/cart.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { SearchComponent } from './search/search.component';
-import { ShopComponent } from './shop/shop.component';
 import { RouterLink } from '@angular/router'
 
 
 
 @NgModule({
   declarations: [
-    AboutComponent,
     HeaderComponent,
     LogoComponent,
     NavigationComponent,
     SearchComponent,
-    ShopComponent
   ],
   imports: [
     CommonModule,
